07c705282e17800f3a19fe46801da574d0bc4806
fwt-bs <almostfelixwu@gmail.com>
2024-08-11 20:25:42 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Example: Toggle a class on the body when the Get Started button is clicked
+document.querySelector('.bg-purple-500').addEventListener('click', function() {
+    document.body.classList.toggle('bg-purple-900');
+    alert('Button clicked!');
+});
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],  // Ensure this path is correct
+  theme: {
+    extend: {
+      colors: {
+        customPurple: '#7c3aed',
+        customBlack: '#000000',
+        customPink: '#FF0077', // Custom pink color
+              },
+            },
+          },
+  plugins: [],
+}
+
+
