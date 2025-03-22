d2f886382a54cfb8395a5c40b8d1a34925cc524a
Joseph Jackson <96963864+jwseph@users.noreply.github.com>
2025-03-22 09:07:28 -0700

    messenger stuff and url fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",  // Include your HTML file(s)
+    "./*.html",  // Include all HTML files in the root directory
+    "./messenger/*.html", // Include all HTML files in messenger directory
   ],
   theme: {
     extend: {
@@ -17,7 +18,19 @@ module.exports = {
     'bg-fuchsia-400',
     'hover:bg-fuchsia-400',
     'bg-fuchsia-500',
-    'hover:bg-fuchsia-500'
+    'hover:bg-fuchsia-500',
+    'max-w-xl',
+    'max-w-2xl',
+    'bg-blue-600',
+    'hover:bg-blue-700',
+    'space-y-2',
+    'list-decimal',
+    'list-disc',
+    'animate-pulse',
+    'bg-violet-500',
+    'hidden',
+    'text-green-500',
+    'text-red-500'
   ],
   plugins: [],
 }
